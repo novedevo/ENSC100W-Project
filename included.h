c595c7c2f40b49fa2d92574f993d3c7c825221dd
Devon Burnham <novedevo@gmail.com>
2019-11-08 16:09:16 -0800

    clarification and minor changes

--- included.h
+++ included.h
@@ -1,19 +1,15 @@
 #include <ESP8266WiFi.h>
 
-void swap(byte *a, byte *b)  {  
+void swap (byte *a, byte *b)  {  
     byte temp = *a;  
     *a = *b;  
     *b = temp;  
 }  
 
-void bubbleSort(byte arr[], int n) {   //borrowed directly from the internet  
+void bubbleSort (byte arr[], int n) {   //borrowed directly from the internet  
     int i, j;  
     for (i = 0; i < n-1; i++)
     for (j = 0; j < n-i-1; j++)  
         if (arr[j] > arr[j+1])  
-            swap(&arr[j], &arr[j+1]);  
-}  
-
-byte getCurrentTime(){
-    return nistToOurTimestamp(get(nist.website:time));
+            swap (&arr[j], &arr[j+1]);  
 }
