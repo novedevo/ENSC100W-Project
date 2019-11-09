45bec59423b97adc994b5db1217b6c99666d3fd7
Devon Burnham <novedevo@gmail.com>
2019-11-09 09:25:36 -0800

    resync

--- deal-with-time.cpp
+++ deal-with-time.cpp
@@ -10,7 +10,23 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 void setup(){
     timeClient.begin();
 }
-byte getCurrentTime(){
+byte getCurrentTimeByte(unsigned long* networkMillis){
     timeClient.update();
+    *networkMillis = millis();
     return(timeClient.getHours() * 10 + timeClient.getMinutes() / 10);
+}
+
+unsigned long timeByteToMillis(byte b, byte n, unsigned long networkMillis){
+    
+    int hours = b/10;
+    int minutes = (b%10)*10;
+    int networkHours = n/10;
+    int networkMinutes = (b%10)*10;
+
+    int deltaHours = hours-networkHours;
+    int deltaMinutes = minutes-networkMinutes;
+
+    int deltaMillis = (deltaHours*60+deltaMinutes)*60*1000;
+    int newMillis = networkMillis + deltaMillis;
+
 }
