0511c5c4d046d83ba7bbba237e9caa3df9e1cce2
Devon Burnham <novedevo@gmail.com>
2019-11-09 11:26:13 -0800

    vastly simplified millis<-> byte conversion

--- scratchplayground.cpp
+++ scratchplayground.cpp
@@ -2,68 +2,22 @@
 //#include <ESP8266WiFi.h>
 using namespace std;
 
-unsigned long timeByteToMillis(unsigned char timeByte, unsigned char networkTimeByte, unsigned long networkMillis){
-    
+unsigned long timeByteToMillis(unsigned char timeByte){
+    if(timeByte>=240){
+        throw "invalidTimeStampError";
+        return 0;
+        }
+
     int hours = timeByte/10;
     int minutes = (timeByte%10)*10;
+    return(hours*60+minutes)*60*1000;
     
-    /*int networkHours = networkTimeByte/10;
-    int networkMinutes = (networkTimeByte%10)*10;
-    int deltaHours;
-    int deltaMinutes;
-*/
-    //if (networkTimeByte>timeByte){
-    //    int bint = 240+timeByte;
-    //    hours = bint/10;
-    //    minutes = (bint%10)*10;
-   // }
-
-    return(hours*60+minutes)*1000;
-
-/*
-    cout<<"networkMillis is: " <<networkMillis<<endl;
-
-    unsigned long millisOffset = networkMillis - (networkHours*60+networkMinutes)*60*1000;
-    cout<<"millis offset is: "<< millisOffset<<endl;
-
-    deltaHours = hours-networkHours;
-    deltaMinutes = minutes-networkMinutes;
-    cout<<"delta hours is: "<<deltaHours<<endl<<"delta minutes is: "<<deltaMinutes<<endl;
-
-    unsigned long deltaMillis = (deltaHours*60+deltaMinutes)*60*1000;
-    cout<<"delta millis is: "<<deltaMillis<<endl;
-
-    unsigned long newMillis = networkMillis + deltaMillis-millisOffset;
-    return (newMillis);*/
 }
 
-unsigned char millisToTimeByte(unsigned long timeInMillis, unsigned char networkTimeByte, unsigned long networkMillis){
-    
+unsigned char millisToTimeByte(unsigned long timeInMillis){
     int hours = timeInMillis/1000/60/60;
     int minutes = (timeInMillis/1000/60)-hours*60;
-
-    cout<<hours;
-
     return (hours*10+minutes/10);
-    /*int networkHours = networkTimeByte/10;
-    int networkMinutes = (networkTimeByte%10)*10;
-
-    unsigned long millisOffset = networkMillis - (networkHours*60+networkMinutes)*60*1000;
-    cout<<"millis offset is: "<< millisOffset<<endl;
-
-    unsigned long deltaMillis = timeInMillis-(networkMillis-millisOffset);
-    cout<<"deltaMillis is: "<< deltaMillis<<endl;
-
-    int deltaHours = deltaMillis/1000/60/60;
-    cout<<"deltaHours is: "<<deltaHours<<endl;
-
-    int deltaMinutes = (deltaMillis/1000/60)-deltaHours*60;
-    cout<<"deltaMinutes is: "<<deltaMinutes<<endl;
-
-    unsigned char deltaTimeByte = deltaHours*10+deltaMinutes/10;
-    unsigned char timeByte = networkTimeByte + deltaTimeByte;
-
-    return (timeByte);*/
 }
 
 int enumerateValues(){
@@ -74,11 +28,16 @@ int enumerateValues(){
 }
 
 int main(){
-    unsigned char b = 100;
+    unsigned char b = 241;
     unsigned char n = 101;
     int daysofnmillis = 4;
     unsigned long l = daysofnmillis*1000*60*60*24;
-    cout <<"current time to millis is: "<< timeByteToMillis(b,n, l)<<endl;
+    //cout <<"current time to millis is: "<< timeByteToMillis(b)<<endl;
     //cout<<endl;
-    cout<< (int)millisToTimeByte(timeByteToMillis(b,n, l), n, l);
+    try{
+        cout<< (int)millisToTimeByte(timeByteToMillis(b));
+    }
+    catch(const char* msg){
+        cerr<<msg;
+    }
 }
