01982d32bd3e44fc1a9c7d168b575491661f042f
Devon Burnham <novedevo@gmail.com>
2019-11-09 11:45:24 -0800

    Added error handling

--- scratchplayground.cpp
+++ scratchplayground.cpp
@@ -2,42 +2,54 @@
 //#include <ESP8266WiFi.h>
 using namespace std;
 
+//takes standard format time of day as byte, returns time of day in milliseconds since 00:00
 unsigned long timeByteToMillis(unsigned char timeByte){
-    if(timeByte>=240){
-        throw "invalidTimeStampError";
-        return 0;
-        }
+    int hours = timeByte / 10;
+    int minutes = (timeByte % 10) * 10;
 
-    int hours = timeByte/10;
-    int minutes = (timeByte%10)*10;
-    return(hours*60+minutes)*60*1000;
+    if (hours >= 24 || minutes >= 60){    //impossible times in standard 24-hours time
+        throw "invalidTimeByte";
+    }
+    
+    unsigned long timeInMillis = (hours * 60 + minutes) * 60 * 1000;
+    return timeInMillis;
     
 }
 
-unsigned char millisToTimeByte(unsigned long timeInMillis){
-    int hours = timeInMillis/1000/60/60;
-    int minutes = (timeInMillis/1000/60)-hours*60;
-    return (hours*10+minutes/10);
+
+//takes time of day in milliseconds since 00:00, returns standard format time of day as a byte
+unsigned char millisToTimeByte(unsigned long timeInMillis){ 
+    int hours = timeInMillis / 1000 / 60 / 60;
+    int minutes = (timeInMillis / 1000 / 60 ) - hours * 60;
+
+    if (hours >= 24 || minutes >= 60){    //impossible times in standard 24-hours time
+        throw "invalidMillisTime";
+    }
+
+    unsigned char timeByte = hours * 10 + minutes / 10;
+    return timeByte;
 }
 
-int enumerateValues(){
+//iterates through all values of a given item, for testing purposes
+int iterateThroughValues(){
     unsigned char c = 0;
-    while (++c>0){
-        cout<<c<<endl;
+    while (++c > 0){
+        cout << c << endl;
     }
 }
 
 int main(){
-    unsigned char b = 241;
-    unsigned char n = 101;
-    int daysofnmillis = 4;
-    unsigned long l = daysofnmillis*1000*60*60*24;
-    //cout <<"current time to millis is: "<< timeByteToMillis(b)<<endl;
-    //cout<<endl;
-    try{
-        cout<< (int)millisToTimeByte(timeByteToMillis(b));
+    unsigned char timeByte = 235;
+    
+    try {
+        cout<< static_cast<int>(millisToTimeByte(timeByteToMillis(timeByte)));
     }
+    
     catch(const char* msg){
-        cerr<<msg;
+        cerr << msg<<endl;
+    }
+    
+    catch(...){
+        cerr << "Unknown Exception"<<endl;
     }
 }
