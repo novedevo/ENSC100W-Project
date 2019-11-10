26373da1c1a627824d05235d8796e6f4b4585e8e
Devon Burnham <novedevo@gmail.com>
2019-11-10 00:14:45 -0800

    resync, refactor

--- main/Server.h
+++ main/Server.h
@@ -1,13 +1,36 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
+#include "Additional.h"
 
 #ifndef SERVER_H
 #define SERVER_H
 
 class Webserver{
   private:
+
+  //WiFiServer server(80);
+  //store HTTP request
+  String header;
+
+  // Auxiliar variables to store the current output state
+  String output5State = "off";
+  String output4State = "off";
+
+  // Assign output variables to GPIO pins
+  const int output5 = 5;
+  const int output4 = 4;
+  Time myTime;
+
+  // Current time
+  unsigned long currentMillisTime = millis();
+  // Previous time
+  unsigned long previousTime = 0; 
+  // Define timeout time in milliseconds (example: 2000ms = 2s)
+  const long timeoutTime = 2000;
+  
   public:
-}
+  void handleClients(WiFiServer* server);
+};
 
 #endif
