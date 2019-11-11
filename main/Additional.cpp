87e04604e9df3eb2d114754a1cd68c39ce04932f
Devon Burnham <novedevo@gmail.com>
2019-11-10 20:53:27 -0800

    resync

--- main/Additional.cpp
+++ main/Additional.cpp
@@ -30,10 +30,17 @@ byte Time::millisToTimeByte(unsigned long timeInMillis) {
   return timeByte;
 }
 
-byte Time::getCurrentNetworkTimeByte(unsigned long* networkMillis, NTPClient timeClient) {
-    timeClient.update();
+byte Time::getCurrentNetworkTimeByte(unsigned long* networkMillis, NTPClient* timeClient) {
+    timeClient->update();
     *networkMillis = millis();
-    return (timeClient.getHours() * 10 + timeClient.getMinutes() / 10);
+    int hours = timeClient->getHours();
+    int minutes = timeClient->getMinutes();
+    byte timeByte = hours*10+minutes/10;
+    if (hours>=24 || minutes>= 60){
+      return (byte)250;
+    }
+    else return timeByte;
+
 }
 
 unsigned long Time::millisAtMidnight(unsigned long networkMillis, unsigned char networkTimeByte) {
@@ -41,99 +48,62 @@ unsigned long Time::millisAtMidnight(unsigned long networkMillis, unsigned char 
   return millisAtMidnight;
 }
 
-/*void myWebserver::handleClients(){
-  WiFiClient client = server.available();   //Listen for incoming clients
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    currentMillisTime = millis();
-    previousTime = currentMillisTime;
-    while (client.connected() && currentMillisTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentMillisTime = millis();         
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) {
-              Serial.println("GPIO 5 on");
-              output5State = "on";
-              digitalWrite(output5, HIGH);
-            } else if (header.indexOf("GET /5/off") >= 0) {
-              Serial.println("GPIO 5 off");
-              output5State = "off";
-              digitalWrite(output5, LOW);
-            } else if (header.indexOf("GET /4/on") >= 0) {
-              Serial.println("GPIO 4 on");
-              output4State = "on";
-              digitalWrite(output4, HIGH);
-            } else if (header.indexOf("GET /4/off") >= 0) {
-              Serial.println("GPIO 4 off");
-              output4State = "off";
-              digitalWrite(output4, LOW);
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 5  
-            client.println("<p>GPIO 5 - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output5State=="off") {
-              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 4 - State " + output4State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output4State=="off") {
-              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
+byte Time::getEstimatedTime(unsigned long networkMillis, byte networkTimeByte){
+  return millisToTimeByte(millis() - millisAtMidnight(networkMillis, networkTimeByte));
+}
+
+byte Time::improvedGetTimeByte(unsigned long* networkMillis, NTPClient* timeClient, byte networkTimeByte){
+  if (WiFi.status() != WL_CONNECTED){
+    return getEstimatedTime(*networkMillis, networkTimeByte);
   }
-}*/
+  else{
+    return getCurrentNetworkTimeByte(networkMillis, timeClient);
+  }
+}
+
+void Time::prepFeedingTimes(byte currentTime, byte (&feedingTimes)[4], bool (&fedTimes)[4]){
+    
+    DataManip datamanip;
+    datamanip.bubbleSort(feedingTimes, 4);
+    for (int i = 0; i<4; i++){
+      if (feedingTimes[i]<currentTime){
+        fedTimes[i] = true;
+        Serial.print(feedingTimes[i]);
+        Serial.println(" is earlier than current time, thus pet has been fed");
+      }
+      else continue;
+    }
+    
+}
+
+void DataManip::swap (byte *a, byte *b)  {
+  byte temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void DataManip::bubbleSort (byte arr[], int n) {
+  int i, j;
+  for (i = 0; i < n - 1; i++)
+    for (j = 0; j < n - i - 1; j++)
+      if (arr[j] > arr[j + 1])
+        swap (&arr[j], &arr[j + 1]);
+}
+
+bool Time::itIsFeedingTime(byte currentTime, byte feedingTimes[4], bool fedTimes[4]){
+  byte index = 5;
+  for (byte i = 0; i<4; i++){
+    if(!(fedTimes[i])&&(feedingTimes[i])){
+      index = i;
+      break;
+      
+    }
+  }
+  Serial.print("Next feeding time is: ");
+  Serial.println(feedingTimes[index]);
+  if (index<4 && feedingTimes[index] <= currentTime){
+    return true;
+  }
+  else return false;
+  
+  }
--- main/Additional.h
+++ main/Additional.h
@@ -8,6 +8,7 @@
 
 class Time{
 private:
+
 public:
 
   //takes standard format time of day as byte, returns time of day in milliseconds since 00:00
@@ -16,9 +17,25 @@ public:
   //takes time of day in milliseconds since 00:00, returns standard format time of day as a byte
   byte millisToTimeByte(unsigned long timeInMillis);
   
-  byte getCurrentNetworkTimeByte(unsigned long* networkMillis, NTPClient timeClient);
+  byte getCurrentNetworkTimeByte(unsigned long* networkMillis, NTPClient* timeClient);
   
   unsigned long millisAtMidnight(unsigned long networkMillis, unsigned char networkTimeByte);  
+
+  byte improvedGetTimeByte(unsigned long* networkMillis, NTPClient* timeClient, byte networkTimeByte);
+
+  byte getEstimatedTime(unsigned long networkMillis, byte networkTimeByte);
+
+  bool itIsFeedingTime(byte currentTime, byte feedingTimes[4], bool fedTimes[4]);
+
+  void prepFeedingTimes(byte currentTime, byte (&feedingTimes)[4], bool (&fedTimes)[4]);
 };
+
+class DataManip{
+  private:
+  public:
+    void swap (byte *a, byte *b);
+    void bubbleSort (byte arr[], int n);
+};
+
 
 #endif
