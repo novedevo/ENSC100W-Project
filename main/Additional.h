8573d2fa07ed282d144cabcbe378b8e6ff795097
Devon Burnham <novedevo@gmail.com>
2021-05-05 18:43:45 -0700

    reformat and simplify source

--- main/Additional.h
+++ main/Additional.h
@@ -1,55 +1,47 @@
-//#include <NTPClient.h>
-//#include <WiFiUdp.h>
-//#include<algorithm>
 #include <ESP8266WiFi.h>
 
 #ifndef ADDITIONAL_H
 #define ADDITIONAL_H
 
-class Time{    //TODO: change class name, change filename?
-
+class Time {
 private:
-  //for internal use only
-  byte index = 5;    //TODO: remove? //? why is this needed
-                      //? perhaps use this to keep track of current feeding time without second array?
-  byte feedingTimes[4];  //TODO: instantiate with default values in CPP file?
-  bool fedTimes[4]; 
+    // for internal use only
+    byte feedingTimes[4];
+    bool fedTimes[4];
 
 public:
-
-  //constructor, takes an array and populates feedingTimes with it
-  Time (const byte (&feedingTimes)[4]){
-    for(int i = 0; i<4; i++){ //* cannot assign arrays to one another directly, must use loop
-      this->feedingTimes[i] = feedingTimes[i];
-      this->fedTimes[i] = 0;
+    // constructor, takes an array and populates feedingTimes with it
+    Time(const byte (&feedingTimes)[4]) {
+        for (int i = 0; i < 4; i++) { //* cannot assign arrays to one another directly, must use loop
+            this->feedingTimes[i] = feedingTimes[i];
+            this->fedTimes[i] = 0;
+        }
     }
-  }
 
-  //Takes comma-deliminated list of exactly four times
-  //Standard time formatting
-  //TODO: handle less than 3 times gracefully?
-  void setFeedingTimes(String timesAsString);
+    // Takes comma-deliminated list of exactly four times
+    // Standard time formatting
+    void setFeedingTimes(String timesAsString);
 
-  //reverses above function
-  String getFeedingTimes();
+    // reverses above function
+    String getFeedingTimes();
 
-  //checks if it is feeding time,
-  //moves on to next feeding time if so
-  bool itIsFeedingTime(byte currentTime);
+    // checks if it is feeding time,
+    // moves on to next feeding time if so
+    bool itIsFeedingTime(byte currentTime);
 
-  //sorts, sets any times earlier than the current time
-  //to "pet has been fed"
-  void prepFeedingTimes(byte currentTime);
+    // sorts, sets any times earlier than the current time
+    // to "pet has been fed"
+    void prepFeedingTimes(byte currentTime);
 
-  //iterates through feedingTimes and prints each value to Serial
-  //for debugging
-  void printFeedingTimes();
+    // iterates through feedingTimes and prints each value to Serial
+    // for debugging
+    void printFeedingTimes();
 
-  //bubblesorts them
-  void sortFeedingTimes();
+    // bubblesorts them
+    void sortFeedingTimes();
 
-  //sets all fedTimes to false, for use at midnight or whenever
-  void resetFedTimes();
+    // sets all fedTimes to false, for use at midnight
+    void resetFedTimes();
 };
 
 #endif
