8a999cb702fecdffca38c0de3ffd1254c4d8ae66
Devon Burnham <novedevo@gmail.com>
2021-05-05 18:43:45 -0700

    reformat and simplify source

--- main/Additional.cpp
+++ main/Additional.cpp
@@ -1,95 +1,88 @@
 #include "Additional.h"
 
-void Time::setFeedingTimes(String timesAsString){
-      const char *str = timesAsString.c_str();  //must be const, otherwise incorrect conversion
-      char* p = strtok((char*)str, ",");  //casts above to a char, then seperates it at comma
-      
-      for(int i = 0; i<4; i++){ //reset arrays
+void Time::setFeedingTimes(String timesAsString) {
+    const char *str = timesAsString.c_str(); // must be const, otherwise incorrect conversion
+    char *p = strtok((char *)str, ","); // casts above to a char, then seperates it at commas
+
+    for (byte i = 0; i < 4; i++) { // reset arrays
         feedingTimes[i] = 0;
         fedTimes[i] = 0;
-      }
-      int index = 0;  //TODO: why is this necessary
-    
-      while (p != nullptr && index < 4) { //while still in our array
-        feedingTimes[index++] = atoi(p);  //atoi converts from char array to integer
-        p = strtok(NULL, ",");  //seperates at character
-        //TODO: understand strtok and atoi better
-      }
-  }
+    }
+    byte index = 0;
 
-String Time::getFeedingTimes(){
-  String returned = "";
-  for (int i = 0; i<3; i++){
-    returned += feedingTimes[i];
-    returned += ",";
-  }
-  //TODO: use strcpy instead of this method
-  returned += feedingTimes[3];
-  
-  return returned;
+    while (p != nullptr && index < 4) { // while still in our array
+        feedingTimes[index++] = atoi(p); // atoi converts from char array to integer
+        p = strtok(NULL, ",");           // seperates at character
+    }
 }
 
-void Time::sortFeedingTimes(){
-    //implementation of bubbleSort
-    int i, j;
+String Time::getFeedingTimes() {
+    String returned = "";
+    for (byte i = 0; i < 3; i++) {
+        returned += feedingTimes[i];
+        returned += ",";
+    }
+    returned += feedingTimes[3];
+
+    return returned;
+}
+
+void Time::sortFeedingTimes() {
+    // bubbleSort
+    byte i, j;
     for (i = 0; i < 4 - 1; i++)
-      for (j = 0; j < 4 - i - 1; j++)
-        if (feedingTimes[j] > feedingTimes[j + 1]){
-          std::swap (feedingTimes[j], feedingTimes[j + 1]);
-          std::swap (fedTimes[j], fedTimes[j + 1]);
+        for (j = 0; j < 4 - i - 1; j++)
+            if (feedingTimes[j] > feedingTimes[j + 1]) {
+                std::swap(feedingTimes[j], feedingTimes[j + 1]);
+                std::swap(fedTimes[j], fedTimes[j + 1]);
+            }
+}
+
+void Time::printFeedingTimes() {
+    for (byte i = 0; i < 4; i++) {
+        Serial.println(feedingTimes[i]);
+    }
+}
+
+void Time::resetFedTimes() {
+    for (byte i = 0; i < 4; i++) {
+        fedTimes[i] = false;
+    }
+}
+
+void Time::prepFeedingTimes(byte currentTime) {
+    sortFeedingTimes();
+      for (byte i = 0; i < 4; i++) {
+        if (feedingTimes[i] < currentTime) {    // if we have already passed this feeding time
+            fedTimes[i] = true; // denote that the pet has been fed
+            Serial.print(feedingTimes[i]);
+            Serial.println(" is earlier than current time, thus pet has been fed");
         }
+    }
 }
 
-void Time::printFeedingTimes(){
-  for (int i = 0; i < 4; i++){
-    Serial.println(feedingTimes[i]);
-  }
-}
+bool Time::itIsFeedingTime(byte currentTime) {
+    byte index = 5; // TODO: why is this here, why does index need to be Time-scoped
+    for (byte i = 0; i < 4; i++) { // using byte to save space, should only ever
+                                  // iterate through 4 values
+        if (!(fedTimes[i]) && (feedingTimes[i])) {
+            index = i;
+            break;
+        }
+    }
 
-void Time::resetFedTimes(){
-  for (int i = 0; i<4; i++){
-    fedTimes[i] = false;
-  }
-}
+    if (index >= 4) { // if, for all times, either the cat has been fed or the time = 0
+        Serial.println("No feeding times until tomorrow.");
+        return false;
+    }
 
-void Time::prepFeedingTimes(byte currentTime){
-  //Serial.println(currentTime);
-  sortFeedingTimes();
-  //printFeedingTimes();
-  for (int i = 0; i<4; i++){
-    //Serial.println(i);
-    if (feedingTimes[i]<currentTime){   //if we have already passed this feeding time
-      fedTimes[i] = true;               //denote that the pet has been fed
-      Serial.print(feedingTimes[i]);  
-      Serial.println(" is earlier than current time, thus pet has been fed");
+    Serial.print("Next feeding time is: ");
+    Serial.println(feedingTimes[index]);
+
+    if (feedingTimes[index] <= currentTime) {        // if the next feeding time is <= the current time
+        fedTimes[index] = true; // denote that the cat has been fed
+        return true;
     } else {
-      //int index = 5;  //TODO: why is this here
-      continue;
+        return false;
     }
-  }
-}
-
-bool Time::itIsFeedingTime(byte currentTime){
-  index = 5;  //TODO: why is this here, why does index need to be Time-scoped
-  for (byte i = 0; i < 4; i++){   //using byte to save space, should only ever iterate through 4 values
-    if(!(fedTimes[i])&&(feedingTimes[i])){
-      index = i;
-      break;
-    }
-  }
-
-  if (index >= 4){  //if, for all times, either the cat has been fed or the time = 0
-    Serial.println("No feeding times until tomorrow.");
-    return false;
-  }
-
-  Serial.print("Next feeding time is: ");
-  Serial.println(feedingTimes[index]);
-
-  if (feedingTimes[index] <= currentTime){  //if the next feeding time is <= the current time
-    fedTimes[index] = true;   //denote that the cat has been fed
-    return true;
-  } else {
-    return false;
-  }
 }
--- main/main.ino
+++ main/main.ino
@@ -1,310 +1,260 @@
-#define BLYNK_TIMEOUT_MS  500  // must be BEFORE BlynkSimpleEsp8266.h doesn't work !!!
-#define BLYNK_HEARTBEAT   17   // must be BEFORE BlynkSimpleEsp8266.h works OK as 17s
-#define BLYNK_PRINT Serial    
+#define BLYNK_TIMEOUT_MS 500 // must be BEFORE BlynkSimpleEsp8266.h
+#define BLYNK_HEARTBEAT                                                                                                                \
+    17 // must be BEFORE BlynkSimpleEsp8266.h. 17 was experimentally determined to
+         // be effective in our situation.
+#define BLYNK_PRINT Serial // used internally by Blynk libraries
 
-#include <TimeLib.h>
-#include <BlynkSimpleEsp8266.h>   //for blynk control over wifi
-#include <WidgetRTC.h>    //for blynk real time clock
-#include "Additional.h"   //Additional code, mainly Time class
-//#include "Server.h"       //additional code to run webserver
-#include <ESP8266WiFi.h>  //basic functionality
+#include "Additional.h"                 //Additional code, mainly Time class
+#include <BlynkSimpleEsp8266.h> //for blynk control over wifi, must only be included in main.ino due to poor library design
+#include <ESP8266WiFi.h>                //basic functionality
 #include <Stepper.h>
+#include <TimeLib.h>
+#include <WidgetRTC.h> //for blynk real time clock
 
-#define extraDebugPrint Serial
+//  example times
+/*  1:00am   =>  010
+    4:30am   =>  043
+    12:00pm  =>  120
+    8:20pm   =>  202
+    1550 hrs =>  155
+    00:00    =>  000
+    5:42am   =>  054
+    5:49am   =>  054
 
-//*       example times 
-/*        1:00am    =>  010
-          4:30am    =>  043
-          12:00pm   =>  120
-          8:20pm    =>  202
-          1550 hrs  =>  155
-          00:00     =>  000
-          5:42am    =>  054
-          5:49am    =>  054
+*  Invalid Times:
+    Anything where the first two digits aren't between 0 and 23 inclusive,
+    or the last digit isn't between 0 and 5 inclusive.
 
-*       Invalid Times:
-          Anything where the first two digits aren't between 0 and 23 inclusive,
-          or the last digit isn't between 0 and 5 inclusive.
-  
-          000 is an invalid time, as it is the default value of our array. 
-          Feed your cat at 23:50 or 00:10.
-  
-          for example:
-          240
-          239
-          006
+    000 is an invalid time, as it is the default value of our array.
+    Feed your cat at 23:50 or 00:10.
+
+    for example:
+    240
+    239
+    006
 
 */
 
-//declaring constants and variables
-const char *ssid     = "DFW";
-const char *password = "***REMOVED***";
-const char *auth = "***REMOVED***";  //auth key for Blynk
+// declaring constants and variables
 
-const byte feedingTimes[4] = {70,120,190,0};  //default initialization
-const bool fedTimes[4] = {0,0,0,0};   //TODO: remove?
+// FILL IN YOUR OWN VALUES HERE
+const char *ssid = "PLACEHOLDER";
+const char *password = "PLACEHOLDER";
+const char *auth = "PLACEHOLDER"; // auth key for Blynk
+
+const byte feedingTimes[4] = {70, 120, 190, 0}; // default initialization
+const bool fedTimes[4] = {0, 0, 0, 0};
 
 const int stepsPerRevolution = 200;
 const int stepperSpeed = 100;
-const int STEPPERPIN2 = 2;  //Labelled as D4 on silkscreen
-const int STEPPERPIN1 = 0;  //Labelled as D3 on silkscreen  //TODO: what pins?
+const int STEPPERPIN2 = 2; // Labelled as D4 on silkscreen
+const int STEPPERPIN1 = 0; // Labelled as D3 on silkscreen
 const int STEPSBACKWARD = -150;
 const int STEPSFORWARD = 300;
 
 const bool debugMode = true;
 
-int numOfTurns = 1;   //number of turns the motor, and thus auger, will make
-                      //equivalent to changing portion size
+int numOfTurns = 1; // number of turns the motor, and thus auger, will make
+                    // equivalent to changing portion size
 int stepCount = 0;
 
-unsigned long currentMillis = 0;    //TODO: remove? replace with millis() calls?
-unsigned long millisOnLastTimeCheck = 0;    //TODO: remove?
-unsigned long millisOnLastBlynkFeeding = 0;    
+unsigned long currentMillis = 0;
+unsigned long millisOnLastTimeCheck = 0;
+unsigned long millisOnLastBlynkFeeding = 0;
 byte currentTime = 0;
-byte fallbackTime = 0;    //TODO: remove?
-byte idealCaseTime = 0;    //TODO: remove?
+byte fallbackTime = 0;
+byte idealCaseTime = 0;
 bool firstRunOnly = true;
 
-bool on = 0;    //TODO: remove?
-bool online = 0;    //TODO: remove?
-
-
+bool on = 0;
+bool online = 0;
 
 //########################################################
-//BETWEEN THESE LINES USED TO BE THE CONTENTS OF ITS OWN HEADER FILE
-//A LIBRARY I WAS USING IS TERRIBLY CODED AND THUS MAY ONLY BE INCLUDED IN THE MAIN INO FILE
+// BETWEEN THESE LINES USED TO BE THE CONTENTS OF ITS OWN HEADER FILE
+// A LIBRARY WE USE IS IMPROPERLY WRITTEN AND THUS MAY ONLY BE INCLUDED IN THE
+// MAIN INO FILE
 //
-//BLINKITYBLINK_H IS DEFINED BELOW
-
-
-
+// BLINKITYBLINK_H IS DEFINED BELOW
 
 class BlinkityBlink : public WidgetRTC {
-  private:
+private:
     byte currentTimeAsByte = 0;
     signed long long millisAsOfMostRecentMidnight = 0;
 
     void resetMidnightMillis();
-
     void updateTime();
-    
-  public:
+
+public:
     byte blynkTimeAsByte();
-    
-    explicit BlinkityBlink() : WidgetRTC() {} //to inherit constructor
-    
+
+    explicit BlinkityBlink() : WidgetRTC() {} // to inherit constructor
     void nightlyReset();
-    
-    void updateBlynkFeedingTimes();   //sends data to blynkapp
+    void updateBlynkFeedingTimes(); // sends data to blynkapp
 };
 
-
-
 //######################
-//END BLINKITYBLINK_H
-//SEE BOTTOM OF FILE FOR BLINKITYBLINK_CPP
+// END BLINKITYBLINK_H
+// SEE BOTTOM OF FILE FOR BLINKITYBLINK_CPP
 
+// Instantiate objects:
 
-
-
-
-//Instantiate objects:
-
-//ConfigServer server(80);  //argument represents the port
-
-Time myTime(feedingTimes);    //TODO: give Time class default initializations?
+Time myTime(feedingTimes);
 BlynkTimer timer;
-//WidgetRTC rtc;      //TODO: remove?
-BlinkityBlink wink;   //* inherits from WidgetRTC
+BlinkityBlink wink; // inherits from WidgetRTC
 Stepper myStepper(stepsPerRevolution, STEPPERPIN1, STEPPERPIN2);
-
-
-
 
 //*###########################
 //*Asynchronous Blynk Functions:
 
-//To be called when Blynk app sends "Feed Now"
-BLYNK_WRITE(V0)
-{
-  currentMillis = millis();
-  if (param.asInt()){
-    if (currentMillis-millisOnLastBlynkFeeding >= 10000 || debugMode){ //if it's been more than 10 seconds since last request
-      spinMotor();
-      millisOnLastBlynkFeeding = currentMillis;
-        if (param.asInt() == 10) {
-          Serial.println("Remote connection from Blynk App ordered pet fed:");
+// To be called when Blynk app sends "Feed Now"
+BLYNK_WRITE(V0) {
+    currentMillis = millis();
+    if (param.asInt()) {
+        if (currentMillis - millisOnLastBlynkFeeding >= 10000 ||
+                debugMode) { // if it's been more than 10 seconds since last request
+            spinMotor();
+            millisOnLastBlynkFeeding = currentMillis;
+            if (param.asInt() == 10) {
+                Serial.println("Remote connection from Blynk App ordered pet fed:");
+            } else {
+                Serial.println("Remote connection from Google Assistant ordered pet fed:");
+            }
+            Serial.println("Running extra feeding cycle..");
         } else {
-          Serial.println("Remote connection from Google Assistant ordered pet fed:");
+            Serial.println("Too little time has elapsed since last feeding.");
+            Serial.println("Taking no action.");
         }
-        Serial.println("Running extra feeding cycle..");
-    } else {
-      Serial.println("Too little time has elapsed since last feeding.");
-      Serial.println("Taking no action.");
     }
-  }
 }
 
-//TODO: why does heartbeat timeout
-
-
-//To be called when Blynk App sends feeding times
-BLYNK_WRITE(V1){
-  myTime.setFeedingTimes(param.asStr());
-  myTime.sortFeedingTimes();
-  myTime.prepFeedingTimes(currentTime); //TODO: implement fron wink class?
-  myTime.printFeedingTimes();
+// To be called when Blynk App sends feeding times
+BLYNK_WRITE(V1) {
+    myTime.setFeedingTimes(param.asStr());
+    myTime.sortFeedingTimes();
+    myTime.prepFeedingTimes(currentTime);
+    myTime.printFeedingTimes();
 }
 
-//To be called when Blynk App sends a number of turns
-BLYNK_WRITE(V2){
-  numOfTurns = param.asInt();
-  Serial.print("Turns changed to ");
-  Serial.println(numOfTurns);
+// To be called when Blynk App sends a number of turns
+BLYNK_WRITE(V2) {
+    numOfTurns = param.asInt();
+    Serial.print("Turns changed to ");
+    Serial.println(numOfTurns);
 }
-
-/*
-*Legacy code from early IFTTT testing
-Was called when google assistant made a request
-now deprecated in favour of different parameters for V0
-TODO: remove?
-*/
-/*
-BLYNK_WRITE(V3){
-  if (param.asInt()){
-    Serial.print("true");
-  } else {Serial.print("false");}
-}
-*/
-
 
 //*################################
-//End Asynchronous Blynk Functions
+// End Asynchronous Blynk Functions
 
-
-
-//Run once whenever the board is reset
-void setup(){  
+// Run once whenever the board is reset
+void setup() {
     Serial.begin(9600);
-    
-    Serial.println(" ");  //because of garbage data written to serial when board is reset
+
+    Serial.println(" "); // because of garbage data written to serial when board is reset
     Serial.print("Connecting to ");
     Serial.println(ssid);
 
-    
     Blynk.begin(auth, ssid, password);
-    CheckConnection();    ////also initiates connection to wifi and blynkapp
-    wink.begin();         //begin tracking time
-    wink.updateBlynkFeedingTimes();   //send feeding times to blynkApp
+    CheckConnection(); // also initiates connection to wifi and blynkapp
+    wink.begin();      // begin tracking time
+    wink.updateBlynkFeedingTimes(); // send feeding times to blynkApp
 
     myStepper.setSpeed(stepperSpeed);
 
-    //Setup timer events
-    //Values are given in milliseconds
+    // Setup timer events
+    // Values are given in milliseconds
     timer.setInterval(60000L, CheckConnection);
     timer.setInterval(10000L, CheckIfItIsTimeToFeedThePet);
     timer.setInterval(60000L, UpdateBlynkFeedingTimes);
-    
 }
 
-void loop(){
+void loop() {
 
-    if(Blynk.connected()){
-      Blynk.run();      //magic blynk code, honestly
-    }
-    
-    timer.run();        //same tbh
-    
-    if (firstRunOnly && year() != 1970){    //Time.lib returns unix time 0 (Jan 1 1970) by default
-      currentTime = wink.blynkTimeAsByte();
-      myTime.prepFeedingTimes(currentTime);
-      firstRunOnly = false;
-    }   //for some reason this didn't work in setup() so it is here
-    
-    //To reset at midnight:
-    //? change to if (!hour()){
-    if (hour() == 0){
-      myTime.resetFedTimes();
+    if (Blynk.connected()) {
+        Blynk.run(); // magic asynchronous blynk code
     }
 
-    //server.handleClients(); //uncomment if webserver functionality is needed
+    timer.run(); //more magic switch
+
+    if (firstRunOnly && year() > 1970) { // Time.lib returns unix time 0 (Jan 1 1970) by default
+        currentTime = wink.blynkTimeAsByte();
+        myTime.prepFeedingTimes(currentTime);
+        firstRunOnly = false;
+    }
+
+    // To reset at midnight:
+    if (hour() == 0) {
+        myTime.resetFedTimes();
+    }
 }
 
-void spinMotor(){
-  for (int i = 0; i<numOfTurns; i++){
-    
-    //digitalWrite(0,HIGH);
-    //delay(1000);      //to communicate with the arduino.
-    
-    myStepper.step(STEPSBACKWARD);
-    yield();                        //If we don't yield, the watchdog bites, throws a stack trace, and resets the module.
-    myStepper.step(STEPSBACKWARD);
-    yield();
-    myStepper.step(STEPSFORWARD);
-    yield();                        //maximum safe length of time at 100 speed is around 500 steps, so we must yield here
-    myStepper.step(STEPSFORWARD);
-    yield();
-  }
-  //digitalWrite (0,LOW); //SEEN AS PIN D3 ON BOARD
+void spinMotor() {
+    for (int i = 0; i < numOfTurns; i++) {
+
+        myStepper.step(STEPSBACKWARD);
+        yield(); // If we don't yield, the watchdog bites, throws a stack trace, and
+                 // resets the module.
+        myStepper.step(STEPSBACKWARD);
+        yield();
+        myStepper.step(STEPSFORWARD);
+        yield(); // maximum safe length of time at 100 speed is around 500 steps, so
+                 // we must yield a lot
+        myStepper.step(STEPSFORWARD);
+        yield();
+    }
 }
 
-void CheckIfItIsTimeToFeedThePet(){
-  Serial.print("Current Blynk time is: ");
-  Serial.println(wink.blynkTimeAsByte());
-  
-  if (myTime.itIsFeedingTime(wink.blynkTimeAsByte())){
-    Serial.println("Thus, it is feeding time!");
-    spinMotor();
-  } else {
-    Serial.println("Thus, it is not feeding time.");
-  }
-}
+void CheckIfItIsTimeToFeedThePet() {
+    Serial.print("Current Blynk time is: ");
+    Serial.println(wink.blynkTimeAsByte());
 
-void CheckConnection(){   // check if connected to Blynk server
-  if(!Blynk.connected()){
-    Serial.println("Not connected to Blynk");
-    if (Blynk.connect(10000)){  //timeout in milliseconds
-      Serial.println("Connected to Blynk!");
-      
+    if (myTime.itIsFeedingTime(wink.blynkTimeAsByte())) {
+        Serial.println("Thus, it is feeding time!");
+        spinMotor();
     } else {
-      Serial.println("Not connected, failing through to backup...");
+        Serial.println("Thus, it is not feeding time.");
     }
-  } else {
-    Serial.println("Connected to Blynk!");
-  }
 }
 
-void UpdateBlynkFeedingTimes(){ //passthrough function because blynkTimer cannot
-                                //take member functions of classes for some reason
-  wink.updateBlynkFeedingTimes();
+void CheckConnection() { // check if connected to Blynk server
+    if (!Blynk.connected()) {
+        Serial.println("Not connected to Blynk");
+        if (Blynk.connect(10000)) { // timeout in milliseconds
+            Serial.println("Connected to Blynk!");
+
+        } else {
+            Serial.println("Not connected, failing through to backup...");
+        }
+    } else {
+        Serial.println("Connected to Blynk!");
+    }
 }
 
-
+void UpdateBlynkFeedingTimes() { // passthrough function because blynkTimer
+                                 // cannot take member functions of classes
+    wink.updateBlynkFeedingTimes();
+}
 
 //*###################
-//BLINKITYBLINK_CPP IS DEFINED BELOW
+// BLINKITYBLINK_CPP IS DEFINED BELOW
 
-
-void BlinkityBlink::resetMidnightMillis(){
-  millisAsOfMostRecentMidnight = millis();
+void BlinkityBlink::resetMidnightMillis() {
+    millisAsOfMostRecentMidnight = millis();
 }
 
-void BlinkityBlink::updateTime(){
-  currentTimeAsByte = hour()*10+minute()/10;  //tricks of integer math
+void BlinkityBlink::updateTime() {
+    currentTimeAsByte = hour() * 10 + minute() / 10; // integer math tricks
 }
 
-byte BlinkityBlink::blynkTimeAsByte(){
-  updateTime();
-  return currentTimeAsByte;
+byte BlinkityBlink::blynkTimeAsByte() {
+    updateTime();
+    return currentTimeAsByte;
 }
 
-void BlinkityBlink::nightlyReset(){
-  resetMidnightMillis();
-}
+void BlinkityBlink::nightlyReset() { resetMidnightMillis(); }
 
-void BlinkityBlink::updateBlynkFeedingTimes(){
-  Blynk.virtualWrite(V1, myTime.getFeedingTimes());
+void BlinkityBlink::updateBlynkFeedingTimes() {
+    Blynk.virtualWrite(V1, myTime.getFeedingTimes());
 }
 
 //*##########################
-//END BLINKITYBLINK_CPP
+// END BLINKITYBLINK_CPP
